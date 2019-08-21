e7668b5a163d328599df2b8f821d00e29487b170
Coding-Corner <kt.goutam@gmail.com>
2019-08-21 17:59:14 +0530

    Create timeout.js

---
+++ NV-JS/Custom-mix/timeout.js
@@ -0,0 +1,42 @@
+setTimeout(function(){
+  $('[id^="navbar-menu"]').hover(function() {
+    $('[id^="navbar-menu"]').css('background', '');
+    $(this).css('background', '#114D89');
+    if(!($(this).attr('id') == "navbar-menu-x")) {
+      var menuElement = $(this).attr('id');
+      var elementIndex = $(this).attr('id').split("-")[2];
+      var subMenuElement = 'sub-menus-wrapper-'+elementIndex;
+      $('#'+subMenuElement).removeClass('bg-transparent');
+      $('#'+subMenuElement).addClass('sub-menu-bg-color');
+      $("[id^='sub-menus-wrapper']").hide();
+      $('#'+subMenuElement).show();
+      $('.content-wrapper').css('margin-top', '4em');
+      $('#'+subMenuElement).hover(function(){
+        $('#'+subMenuElement).removeClass('bg-transparent');
+        $('#'+subMenuElement).addClass('sub-menu-bg-color');
+        $('#'+menuElement).addClass("sub-menu-bg-color");
+        $('#'+subMenuElement).show();
+        $('.content-wrapper').css('margin-top', '4em');
+      }
+                                  ,function() {
+                                    $('#'+subMenuElement).removeClass('sub-menu-bg-color');
+                                    $('#'+subMenuElement).addClass('bg-transparent');
+                                    $('#'+menuElement).removeClass("sub-menu-bg-color");
+                                    $('#'+subMenuElement).hide();
+                                    $('.content-wrapper').css('margin-top', '4em');
+                                  }
+                                 );
+    }
+  }
+                                 , function() {
+                                   var menuelement = $(this).attr('id');
+                                   var elementIndex = $(this).attr('id').split("-")[2];
+                                   var subMenuElement = 'sub-menus-wrapper-'+elementIndex;
+                                   $('#'+subMenuElement).removeClass('sub-menu-bg-color');
+                                   $('#'+subMenuElement).addClass('bg-transparent');
+                                   $('#'+subMenuElement).hide();
+                                   $('.content-wrapper').css('margin-top', '4em');
+                                 }
+                                );
+}
+           ,2000);
